b3f018ffcc65470101d123375198bd55470910fd
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-05-09 16:07:18 -0400

    gitQ

---
+++ gitQuery/build/precache-manifest.bdd30819c3fcb4689fa3326843ad3968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c00847d9d96799945a72",
+    "url": "/static/js/main.870a4f59.chunk.js"
+  },
+  {
+    "revision": "a863c4339dd0ac7ca92c",
+    "url": "/static/js/2.f3f86122.chunk.js"
+  },
+  {
+    "revision": "c00847d9d96799945a72",
+    "url": "/static/css/main.af0995d9.chunk.css"
+  },
+  {
+    "revision": "a863c4339dd0ac7ca92c",
+    "url": "/static/css/2.f6c3e9e5.chunk.css"
+  },
+  {
+    "revision": "a7772a0ecfa2c848a20f2d8c4e266462",
+    "url": "/index.html"
+  }
+];
